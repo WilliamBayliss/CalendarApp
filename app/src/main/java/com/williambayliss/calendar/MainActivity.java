75a0ad4946a45fad88efce461141520c74e61943
williambayliss <wmbayliss@gmail.com>
2021-02-14 16:51:58 -0500

    added event, template, settings buttons to MainActivity

--- app/src/main/java/com/williambayliss/calendar/MainActivity.java
+++ app/src/main/java/com/williambayliss/calendar/MainActivity.java
@@ -3,14 +3,21 @@ package com.williambayliss.calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
+    Button newEventButton;
+    Button newTemplateButton;
+    Button settingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calendarView = findViewById(R.id.calendar_view);
+        calendarView = findViewById(R.id.calendarView);
+        newEventButton = findViewById(R.id.newEventButton);
+        newTemplateButton = findViewById(R.id.newTemplateButton);
+        settingsButton = findViewById(R.id.settingsButton);
     }
 }
