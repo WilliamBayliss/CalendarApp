66b80e2bca7d997ea7c9f226b1a07419ba44f135
williambayliss <wmbayliss@gmail.com>
2021-02-15 18:40:56 -0500

    Hooked up template_activity.xml buttons to activity code

--- app/src/main/java/com/williambayliss/calendar/TemplateActivity.java
+++ app/src/main/java/com/williambayliss/calendar/TemplateActivity.java
@@ -3,23 +3,38 @@ package com.williambayliss.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TemplateActivity extends AppCompatActivity {
-    TextView titleTextView;
-
+    EditText eventTitle;
+    EditText eventLocation;
+    TimePicker fromTime;
+    TimePicker untilTime;
+    Button alertsButton;
+    Button saveButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         setContentView(R.layout.template_activity);
 
-        titleTextView = findViewById(R.id.newTemplateTextView);
+        eventTitle = findViewById(R.id.eventTitleEditText);
+        eventLocation = findViewById(R.id.eventLocationEditText);
+        fromTime = findViewById(R.id.fromTimeSpinner);
+        untilTime = findViewById(R.id.untilTimeSpinner);
+        alertsButton = findViewById(R.id.alertsButton);
+        saveButton = findViewById(R.id.saveButton);
+
+
+
 
     }
 }
