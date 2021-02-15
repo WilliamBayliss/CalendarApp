f72db1eacd03ae6fcbaa33831b910ac482606161
williambayliss <wmbayliss@gmail.com>
2021-02-14 19:07:09 -0500

    added newTemplate activity

--- app/src/main/java/com/williambayliss/calendar/MainActivity.java
+++ app/src/main/java/com/williambayliss/calendar/MainActivity.java
@@ -1,12 +1,19 @@
 package com.williambayliss.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 
+import java.util.EventListener;
+
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout layout;
     CalendarView calendarView;
     Button newEventButton;
     Button newTemplateButton;
@@ -15,9 +22,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.constraintLayout);
         calendarView = findViewById(R.id.calendarView);
         newEventButton = findViewById(R.id.newEventButton);
         newTemplateButton = findViewById(R.id.newTemplateButton);
         settingsButton = findViewById(R.id.settingsButton);
+
+        newTemplateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewTemplate();
+            }
+        });
+    }
+
+    public void startNewTemplate() {
+        Intent intent = new Intent(this, TemplateActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/williambayliss/calendar/TemplateActivity.java
@@ -0,0 +1,22 @@
+package com.williambayliss.calendar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TemplateActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Intent intent = getIntent();
+        recyclerView = findViewById(R.id.templateRecyclerView);
+
+    }
+}
